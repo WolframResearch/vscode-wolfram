713c5e3fb062f746e81fb4dda8497cce4b9ac4b2
Brenton Bostick <brenton@wolfram.com>
2019-05-08 08:54:49 -0500

    Draft 0.11 updates

--- README.md
+++ README.md
@@ -1,21 +1,96 @@
+
 # wolfram-language README
 
 
-## setup
+# LSPServer
 
+Follow instructions for LSPServer paclet.
+
+
+
+
+
+
+
+# Setup
+
+Download the VSIX file from:
+
+`http://temp-store.wolfram.com/temp-store/brenton/wolfram-vscode-releases/`
+
+
+Open VS Code
+
+Open the Command Palette (⇧⌘P)
+
+Enter the command:
+Extensions: Install from VSIX
+
+Install the VSIX file that you downloaded.
+
+
+
+Open the Command Palette
+
+Enter the command:
 Preferences: Configure Language Specific Settings...
 
-in settings.json put:
+Select Wolfram
+
+
+A settings.json file is now open.
+
+In settings.json put:
 
 ```
 {
     "[wolfram]": {
         "wolframkernel": "/path/to/my/wolframkernel",
-        "proxy": "/path/to/my/wolfram_lsp_proxy.py"
+        "proxy": "/path/to/my/wolfram_lsp_proxy.py",
+        "logDir": "/path/to/my/log/dir"
     }
 }
 ```
 
+### Windows
+
+
+It is recommended to specify wolfram.exe instead of WolframKernel.exe.
+
+WolframKernel.exe opens a new window while it is running. But wolfram.exe runs inside the window that started it.
+
+
+
+
+
+
+Restart VS Code
+
+
+
+You should now have syntax highlighting and linting of Wolfram .m and .wl files working.
+
+Test this by typing this into a .m file and saving it:
+
+```
+Which[a, b, a, b]
+```
+
+You should see warnings about duplicate clauses.
+
+
+
+
+
+
+
+
+# Troubleshooting
+
+## I see "Unknown Identifier. Use language identifiers" while editing settings.json
+
+This is an open issue:
+https://github.com/microsoft/vscode/issues/26707
 
 
 
--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,7 +1,3 @@
-/* --------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- * ------------------------------------------------------------------------------------------ */
 
 import * as path from 'path';
 import { workspace, ExtensionContext, WorkspaceConfiguration } from 'vscode';
@@ -16,13 +12,6 @@ import {
 let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
-	// The server is implemented in node
-	//let serverModule = context.asAbsolutePath(
-	//	path.join('server', 'out', 'server.js')
-	//);
-	// The debug options for the server
-	// --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-	//let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
 
 	const config: WorkspaceConfiguration = workspace.getConfiguration("[wolfram]", null);
 
@@ -30,28 +19,30 @@ export function activate(context: ExtensionContext) {
 	let wolframkernel: string = config.get<string>("wolframkernel");
 	let logDir: string = config.get<string>("logDir");
 
-	// If the extension is launched in debug mode then the debug server options are used
-	// Otherwise the run options are used
+	let args;
+	if (logDir) {
+		args = [proxy, wolframkernel, "--logDir", logDir]
+	} else {
+		args = [proxy, wolframkernel]
+	}
+
 	let serverOptions: ServerOptions = {
 		run: {
 			transport: TransportKind.stdio,
-			command: "python3", args: [proxy, wolframkernel, "--debug", "--logDir", logDir] },
+			command: "python",
+			args: args
+		},
 		debug: {
 			transport: TransportKind.stdio,
-			command: "python3", args: [proxy, wolframkernel, "--debug", "--logDir", logDir] }
-	};
-
-	// Options to control the language client
-	let clientOptions: LanguageClientOptions = {
-		// Register the server for plain text documents
-		documentSelector: [{ scheme: 'file', language: 'wolfram' }],
-		synchronize: {
-			// Notify the server about file changes to '.clientrc files contained in the workspace
-			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
+			command: "python",
+			args: args
 		}
 	};
 
-	// Create the language client and start the client.
+	let clientOptions: LanguageClientOptions = {
+		documentSelector: [{ scheme: 'file', language: 'wolfram' }],
+	};
+
 	client = new LanguageClient(
 		'wolfram',
 		'Wolfram Language',
@@ -59,7 +50,6 @@ export function activate(context: ExtensionContext) {
 		clientOptions
 	);
 
-	// Start the client. This will also launch the server
 	client.start();
 }
 
