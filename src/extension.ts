de457f3564e3e7c1d2cc97e5f22e2a9efd620bc2
Brenton Bostick <brenton@wolfram.com>
2020-12-08 09:00:12 -0600

    Enable implicitTokens in VSCode

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,15 @@
 
-import * as path from 'path';
-import { workspace, ExtensionContext, WorkspaceConfiguration } from 'vscode';
-import { window } from 'vscode';
-
+import { basename } from 'path';
+import {
+	workspace,
+	window,
+	DecorationOptions,
+	ExtensionContext,
+	Range,
+	TextEditorDecorationType,
+	WorkspaceConfiguration,
+	Position
+} from 'vscode';
 import {
 	LanguageClient,
 	LanguageClientOptions,
@@ -10,9 +17,47 @@ import {
 	TransportKind
 } from 'vscode-languageclient';
 
+
+interface ImplicitTokenI {
+	line: number;
+	column: number;
+	character: string
+}
+
+interface ImplicitTokensI {
+	uri: string;
+	tokens: ImplicitTokenI[]
+}
+
+
+// interface HTMLSnippetLineI {
+// 	line: number;
+// 	content: string;
+// 	characterCount: number
+// }
+
+// interface HTMLSnippetActionI {
+// 	command: number;
+// 	insertionText: string;
+// 	deletionText: string;
+// 	line: number;
+// 	column: number;
+// 	href: string
+// }
+
+// interface HTMLSnippetI {
+// 	uri: string;
+// 	lines: HTMLSnippetLineI[]
+// 	actions: HTMLSnippetActionI[]
+// }
+
+
 let client: LanguageClient;
 
 let kernel_initialized = false;
+
+let implicitTokensDecorationType = window.createTextEditorDecorationType({});
+
 
 export function activate(context: ExtensionContext) {
 
@@ -41,6 +86,10 @@ export function activate(context: ExtensionContext) {
 		command[0] = kernel
 	}
 
+	let implicitTokens = config.get<boolean>("implicitTokens", false);
+	// let bracketMatcher = config.get<boolean>("bracketMatcher", false);
+	// let debugBracketMatcher = config.get<boolean>("debugBracketMatcher", false);
+
 	let base = basename(command[0]);
 
 	if (!base.toLowerCase().startsWith("wolframkernel")) {
@@ -62,9 +111,13 @@ export function activate(context: ExtensionContext) {
 
 	let clientOptions: LanguageClientOptions = {
 		documentSelector: [{ scheme: 'file', language: 'wolfram' }],
-		initializationOptions: {}
+		initializationOptions: {
+			implicitTokens: implicitTokens,
+			// bracketMatcher: bracketMatcher,
+			// debugBracketMatcher: debugBracketMatcher
+		}
 	};
-
+	
 	client = new LanguageClient(
 		'wolfram',
 		'Wolfram Language',
@@ -73,13 +126,84 @@ export function activate(context: ExtensionContext) {
 	);
 
 	client.onReady().then(() => {
-		kernel_initialized = true
-	  });
 
+		kernel_initialized = true;
+
+		client.onNotification("textDocument/publishImplicitTokens", (params: ImplicitTokensI) => {
+
+			let activeEditor = window.activeTextEditor;
+
+			if (!activeEditor) {
+				return;
+			}
+
+			let opts: DecorationOptions[] = [];
+
+			params.tokens.forEach( (t) => {
+				if (!activeEditor) {
+					return;
+				}
+
+				const opt: DecorationOptions = {
+					range: new Range(new Position(t.line - 1, t.column - 1), new Position(t.line - 1, t.column - 1)),
+					renderOptions: {
+						before: {
+							contentText: implicitTokenCharToText(t.character),
+							color: 'gray'
+						}
+					}
+				};
+
+				opts.push(opt);
+			})
+			
+			// activeEditor.setDecorations(implicitTokensTimesDecorationType, implicitTokensTimes);
+			// activeEditor.setDecorations(implicitTokensOneDecorationType, implicitTokensOne);
+			// activeEditor.setDecorations(implicitTokensAllDecorationType, implicitTokensAll);
+			// activeEditor.setDecorations(implicitTokensNullDecorationType, implicitTokensNull);
+			activeEditor.setDecorations(implicitTokensDecorationType, opts);
+		});
+
+		// client.onNotification("textDocument/publishHTMLSnippet", (params: HTMLSnippetI) => {
+			// experimental.appendLine("publishHTMLSnippet: ");
+			// experimental.appendLine("uri: " + params.uri);
+			// params.actions.forEach( (a) => {
+			// 	experimental.appendLine("action: ");
+			// 	experimental.appendLine("    " + a.column);
+			// 	experimental.appendLine("    " + a.command);
+			// 	experimental.appendLine("    " + a.deletionText);
+			// 	experimental.appendLine("    " + a.href);
+			// 	experimental.appendLine("    " + a.insertionText);
+			// 	experimental.appendLine("    " + a.line);
+			// })
+			// params.lines.forEach( (l) => {
+			// 	experimental.appendLine("line: ");
+			// 	experimental.append("    " + l.characterCount);
+			// 	experimental.append("    " + l.content);
+			// 	experimental.append("    " + l.line);
+			// })
+			// experimental.appendLine("done publishHTMLSnippet");
+		// });
+	  });
 	
 	setTimeout(kernel_initialization_check_function, 10000, command);
 
 	client.start();
+}
+
+function implicitTokenCharToText(c: string) {
+	switch (c) {
+		case "x": return "\xd7";
+		case "N": return "Null";
+		case "1": return "1";
+		case "A": return "All";
+		case "e": return "\u25a1";
+		case "f": return "\u25a1\xd7";
+		case "y": return "\xd71";
+		case "B": return "All\xd7";
+		case "C": return "All\xd71";
+		default: return " ";
+	}
 }
 
 function kernel_initialization_check_function(command: [string]) {
