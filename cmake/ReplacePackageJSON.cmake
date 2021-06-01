daef3f9a029a79b95326e01a1e8210ddeae4a95c
Brenton Bostick <brenton@wolfram.com>
2021-06-01 10:28:26 -0500

    Create package-lock.json during build process
    Also use @@ for easier regex replacement

--- cmake/ReplacePackageJSON.cmake
+++ cmake/ReplacePackageJSON.cmake
@@ -3,8 +3,28 @@ file(READ ${GENERATED_PACKAGEJSON} filedata)
 
 if(LOCAL_BUILD)
 
-string(REGEX REPLACE "\"version\": \"[0-9]+\\.[0-9]+\\.[0-9]+\"" "\"version\": \"${LOCAL_BUILD_VERSION}\"" filedata ${filedata})
+string(REGEX REPLACE "\"version\": \"@([0-9]+\\.[0-9]+\\.[0-9]+)@\"" "\"version\": \"${LOCAL_BUILD_VERSION}\"" filedata ${filedata})
+
+else()
+
+string(REGEX REPLACE "\"version\": \"@([0-9]+\\.[0-9]+\\.[0-9]+)@\"" "\"version\": \"\\1\"" filedata ${filedata})
 
 endif()
 
 file(WRITE ${GENERATED_PACKAGEJSON} "${filedata}")
+
+
+
+file(READ ${GENERATED_PACKAGELOCKJSON} filedata)
+
+if(LOCAL_BUILD)
+
+string(REGEX REPLACE "\"version\": \"@([0-9]+\\.[0-9]+\\.[0-9]+)@\"" "\"version\": \"${LOCAL_BUILD_VERSION}\"" filedata ${filedata})
+
+else()
+
+string(REGEX REPLACE "\"version\": \"@([0-9]+\\.[0-9]+\\.[0-9]+)@\"" "\"version\": \"\\1\"" filedata ${filedata})
+
+endif()
+
+file(WRITE ${GENERATED_PACKAGELOCKJSON} "${filedata}")
