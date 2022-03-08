6f0f20a421e67652f6e1c9eaa7b8a30621b020a4
Brenton Bostick <brenton@wolfram.com>
2022-03-08 07:57:12 -0600

    Remove command for uninstalling with old name

--- cmake/InstallVSIX.cmake
+++ cmake/InstallVSIX.cmake
@@ -11,11 +11,6 @@ endif()
 #
 execute_process(
   COMMAND
-    #
-    # uninstall the old name for some indefinite time into the future
-    #
-    ${CODE_COMMAND} --uninstall-extension brenton.wolfram
-  COMMAND
     ${CODE_COMMAND} --uninstall-extension WolframResearch.wolfram
 )
 
