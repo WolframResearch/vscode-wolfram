9edfe6b2ab29a3044d137dd7c0693e6e41067410
Suman Banerjee <sumanb@wolfram.com>
2024-11-19 19:39:02 +0530

    Docs update.

--- cmake/ReplaceREADME.cmake
+++
@@ -1,6 +0,0 @@
-
-file(READ ${COPIED_README} filedata)
-
-string(REGEX REPLACE "<!--  filter me START -->.*<!--  filter me END -->" "" filedata ${filedata})
-
-file(WRITE ${COPIED_README} "${filedata}")
